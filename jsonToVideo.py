e80d2b5e3fd91fae960a0acd66392fbd8f431cb0
Neethamadhu <neethamadhu.ma@gmail.com>
2023-08-12 03:15:20 +0530

    Fix frame recreation using coodinates

--- jsonToVideo.py
+++ jsonToVideo.py
@@ -3,16 +3,30 @@ import mediapipe as mp
 import numpy as np
 import json
 
+HEIGHT = 720
+WIDTH = 720
+
+def map_to_range(number):
+    # Ensure the input number is within the range -1 to 1
+    if number < -1:
+        number = -1
+    elif number > 1:
+        number = 1
+    
+    # Map the number to the range 0 to 1
+    mapped_number = (number + 1) / 2
+    return mapped_number
+
 def jsonToVid(data):
     
     mp_pose = mp.solutions.pose
     mp_drawing = mp.solutions.drawing_utils
     pose = mp_pose.Pose()
-    cap = cv2.VideoCapture('sit_31.mp4')
+    cap = cv2.VideoCapture('break_31.avi')
 
     output_file = 'outputFromMobileJson.mp4'
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_file, fourcc, 30, (1280, 720))
+    out = cv2.VideoWriter(output_file, fourcc, 30, (WIDTH, HEIGHT))
 
     ret, frame = cap.read()
     results = pose.process(frame)
@@ -20,12 +34,12 @@ def jsonToVid(data):
     l = []
 
     for dt1 in data:
-        black_screen = np.zeros((1280,720, 3), dtype=np.uint8)
+        black_screen = np.zeros((WIDTH,HEIGHT, 3), dtype=np.uint8)
         i = 0
         for dt in dt1:
-            results.pose_landmarks.landmark[i].x = dt['x']
-            results.pose_landmarks.landmark[i].y = dt['y']
-            results.pose_landmarks.landmark[i].z = dt['z']
+            results.pose_landmarks.landmark[i].x = map_to_range(dt['x'])
+            results.pose_landmarks.landmark[i].y = map_to_range(dt['y'])
+            results.pose_landmarks.landmark[i].z = map_to_range(dt['z'])
             i = i + 1
 
         mp_drawing.draw_landmarks(black_screen, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
@@ -33,11 +47,12 @@ def jsonToVid(data):
                             mp_drawing.DrawingSpec((255, 0, 255), 2, 3)
                             )
         
+        black_screen = np.rot90(black_screen, k=1)
         cv2.imshow('MediaPipe Pose', black_screen)
 
         cv2.waitKey(0)
 
-        break
+        # break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
